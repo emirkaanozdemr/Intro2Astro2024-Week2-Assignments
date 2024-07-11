4711c9440429ff0f09c9292135737486f9e72308
Emir Kaan Özdemir <155099794+emirkaanozdemr@users.noreply.github.com>
2024-07-11 19:51:15 +0300

    Create assignment-1.py

---
+++ assignment-1.py
@@ -0,0 +1,16 @@
+## Problem: Calculate the gravitational force between a 3 solar mass star and its 10 Jupyter mass planet 2 au away.
+
+import astropy.units as u
+import astropy.constants as c
+
+sun=c.M_sun
+star=sun*3
+jupyter=c.M_jup
+planet=jupyter*10
+distance = 2 * u.au
+
+
+force=(u.G)*star*planet/distance**2
+
+force_in_newtons=force.to(u.N)
+print(force_in_newtons)
